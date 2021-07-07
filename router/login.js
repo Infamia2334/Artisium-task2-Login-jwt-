566e7bd1c73b9d874dbf36588dcc7221501be553
Infamia2334 <53487995+Infamia2334@users.noreply.github.com>
2021-07-07 12:41:22 +0530

    auth and redirect working

--- router/login.js
+++ router/login.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const User = require("../models/User")
-
+const auth = require("../middleware/auth")
 
 router.get("/", (req, res)=>{
     res.render("signin")
@@ -10,14 +10,33 @@ router.get("/", (req, res)=>{
 router.post("/",async (req, res)=>{
     try{
         const user = await User.findByCredentials(req.body.username, req.body.password)
+        //generating auth token
         const token = await user.generateAuthToken()
-        res.status(200).send({user: user, token: token})
+      
+        console.log({user: user, token: token})
+        //Setting cookie
+        res.cookie("token", token, {maxAge: process.env.TOKEN_LIFE * 1000})
+        if(user){
+            res.redirect("/login/dashboard")
+        }
+        res.end()
+
     }
     catch(e){
         console.log(e)
         res.status(400).send()
     }
     
+})
+
+router.get("/dashboard", auth, async (req, res)=>{
+    try{
+      
+        res.render("live")
+    }
+    catch(err){
+        res.send(err)
+    }
 })
 
 module.exports = router
