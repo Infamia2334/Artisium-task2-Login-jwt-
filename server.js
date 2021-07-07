2b4ac005d193bc31cab5690d65df483a9c9952d8
Infamia2334 <53487995+Infamia2334@users.noreply.github.com>
2021-07-07 12:43:23 +0530

    Cookie parser package added

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const dotenv = require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 const path = require("path")
-
+const cookieParser = require("cookie-parser")
 
 const app = express()
 const port = process.env.PORT || 3000
 
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true,useUnifiedTopology:true,useFindAndModify: true, useCreateIndex: true}).then(()=>console.log("Connected to DB successfully")).catch(err => console.log(err))
+
+// app.use((req, res, next)=>{
+//     console.log(req.method, req.path)
+//     next()
+// })
+
+
 app.use(express.json())
+app.use(cookieParser())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("public"))
 app.use("/css", express.static(__dirname  + "public/styles"))
