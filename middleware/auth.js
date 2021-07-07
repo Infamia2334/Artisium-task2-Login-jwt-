4395baafca1bf9587e44707ff77d9696573be6c3
Infamia2334 <53487995+Infamia2334@users.noreply.github.com>
2021-07-07 12:41:48 +0530

    Added new jwt authentication middleware

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken")
+const User = require("../models/User")
+
+const auth = async (req, res, next)=> {
+    try{
+        // const token = req.cookies("Authorization").replace("Bearer", "").trim()
+        const token = req.cookies.token
+        console.log(token)
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
+        const user = await User.findOne({ _id: decoded._id, "tokens.token": token })
+
+
+        if(!user){
+            throw new Error()
+        }
+
+        req.user = user
+        next()
+
+    } catch(err){
+        console.log(err)
+        res.status(401).send({ error: "Error when authenticating"})
+    }
+}
+
+
+module.exports = auth
