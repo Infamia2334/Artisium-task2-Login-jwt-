ed58b9a55d86b6d812bdf6007987e2669cc70852
Infamia2334 <53487995+Infamia2334@users.noreply.github.com>
2021-07-09 16:11:06 +0530

    Refactored and cleaned login route

--- router/login.js
+++ router/login.js
@@ -1,12 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const mongoose = require("mongoose")
-const User = require("../models/User")
-const auth = require("../middleware/auth")
-const moment = require("moment")
-const Plc = require("../server")
 
-const dateFormat = "YYYY-MM-DD HH:mm"
+const auth = require("../middleware/auth")
+
+const login = require("../middleware/controllers/login")
+const liveData = require("../middleware/controllers/dashboard")
+
 
 
 
@@ -14,96 +13,21 @@ router.get("/", (req, res)=>{
     res.render("signin")
 })
 
-router.post("/",async (req, res)=>{
+router.post("/", login)
+
+router.get("/dashboard", auth, liveData)
+
+router.post("/logout", auth, async (req, res)=>{
     try{
-        const user = await User.findByCredentials(req.body.username, req.body.password)
-        //generating auth token
-        const token = await user.generateAuthToken()
-      
-        console.log({user: user, token: token})
-        //Setting cookie
-        res.cookie("token", token, {maxAge: process.env.TOKEN_LIFE * 1000})
-        if(user){
-            res.redirect("/login/dashboard")
-        }
-        res.end()
+        req.user.tokens = req.user.tokens.filter((token)=>{
+            return token.token !== req.token
+        })
+        await req.user.save()
 
-    }
-    catch(e){
-        console.log(e)
-        res.status(400).send()
-    }
-    
-})
+        res.send()
 
-
-
-router.get("/dashboard", auth, async (req, res)=>{
-   
-            // const data = await Plc.find({}, {data: 1, inserted_time: 1, _id: 0}).sort({$natural: -1}).limit(1)
-            
-            //     let result = JSON.stringify(data[0])
-                
-            //     let d = JSON.parse(result)
-            //     let p = d["data"]
-            //      res.render("live", {PLC:p})
-                // res.status(200).json({
-                //     status: "success",
-                //     data: p
-                // })
-                
-            // }); 
-    try{
-        await Plc.find({},{data:1, _id: 0, inserted_time: 1}, (err, found)=>{
-            // options:{
-            //     limit: 10
-            // }
-            if(!err){
-                // console.log(found)
-                var d
-                var date
-                var dateTime
-                if(found.length){
-                    var keys = []
-                    var data_values = []
-                    found.forEach(e => {
-                     
-                    
-                    let result = JSON.stringify(e)
-                    
-                    // var result = JSON.stringify(found[e])
-                    console.log(result)
-                    d = JSON.parse(result)
-                    console.log(d.data.temperature)
-                    
-                    for(var k in d.data){
-                        keys.push(k)
-                        data_values.push(d.data[k])
-                        date = new Date(e.inserted_time)
-                        console.log(date)
-                        dateTime = moment(date).format(dateFormat)
-                    }
-                    
-                    
-                    });
-                    console.log(keys)
-                    console.log(data_values)
-                    // res.send(found)
-                    res.render("live", {timestamp: dateTime, data_keys: keys, data_values: data_values})
-                }
-                else{
-                    console.log("no data in db")
-                }
-                
-            }
-            else{
-                res.send(err)
-            }
-        }).limit(10)
-        // res.render("live", {PLC: found})
-    }
-    catch(err){
-        res.send(err)
+    } catch(e){
+        res.status(500).send()
     }
 })
 
